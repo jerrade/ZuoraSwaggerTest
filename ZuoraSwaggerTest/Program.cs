c55c242a18c393f0b4b59328ecdc4f90e83e6672
Jerrad Elmore <Jerrad.Elmore@tobiidynavox.com>
2017-01-24 09:04:01 -0500

    First commit

---
+++ ZuoraSwaggerTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZuoraSwaggerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZuoraService zuoraService = new ZuoraService("username", "password", "https://rest.apisandbox.zuora.com/v1");
+
+            /* This works.  The "account" variable ends up storing the account record requested. */
+            var account = zuoraService.GetAccount("4028e69733156f3c01331cd8249458f1");  // <----Use valid account id here
+            Console.WriteLine(account);
+
+            /* This doesn't work.  The "query" variable is null, even though the API returns data.  Put a breakpoint on line 1388 in ActionsApi.cs and inspect the localVarResponse variable. */
+            var query = zuoraService.Query("select Id, CreatedDate from Account where Id = '4028e69733156f3c01331cd8249458f1'");
+            Console.WriteLine(query);
+        }
+    }
+}
---
+++ ZuoraSwaggerTest/ZuoraService.cs
@@ -0,0 +1,48 @@
+﻿using IO.Swagger.Api;
+using IO.Swagger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZuoraSwaggerTest
+{
+    public class ZuoraService
+    {
+        private string username;
+        private string password;
+        private string serviceURL;
+
+        private AccountsApi accountsAPI;
+        private ActionsApi actionsAPI;
+
+        public ZuoraService(string username, string password, string serviceURL)
+        {
+            this.username = username;
+            this.password = password;
+            this.serviceURL = serviceURL;
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            this.accountsAPI = new AccountsApi(this.serviceURL);
+            this.accountsAPI.Configuration.ApiClient.RestClient.Authenticator = new RestSharp.Authenticators.HttpBasicAuthenticator(this.username, this.password);
+
+            this.actionsAPI = new ActionsApi(this.serviceURL);
+            this.actionsAPI.Configuration.ApiClient.RestClient.Authenticator = new RestSharp.Authenticators.HttpBasicAuthenticator(this.username, this.password);
+        }
+
+        public GETAccountType GetAccount(string id)
+        {
+            var account = this.accountsAPI.GETAccount(id);
+            return account;
+        }
+
+        public ProxyActionqueryResponse Query(string queryString)
+        {
+            ProxyActionqueryRequest request = new ProxyActionqueryRequest(queryString);
+            var response = this.actionsAPI.ProxyActionPOSTquery(request);
+            return response;
+        }
+    }
+}
